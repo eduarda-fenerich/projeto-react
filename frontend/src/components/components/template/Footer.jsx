4d43f3a9be239419b0182a74d3c124a70f4ef837
eduarda-fenerich <fenericheduarda13@gmail.com>
2025-11-04 09:38:48 -0300

    Add files via upload

--- frontend/src/components/components/template/Footer.jsx
+++ frontend/src/components/components/template/Footer.jsx
@@ -1,2 +1,12 @@
 import './Footer.css';
 import React from 'react';
+
+const Footer = () => (
+    <footer className='footer'>
+        <span>
+            Desenvolvido por Eduarda
+        </span>
+    </footer>
+);
+
+export default Footer;
